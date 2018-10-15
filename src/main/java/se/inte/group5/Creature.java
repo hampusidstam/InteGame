07cd5e9048d186b364b16dc08521770ac02cdb81
loha4254 <lovisa.hanserkers@gmail.com>
2018-10-15 16:26:52 +0200

    Test cases for maxSpeed created

--- src/main/java/se/inte/group5/Creature.java
+++ src/main/java/se/inte/group5/Creature.java
@@ -1,6 +1,18 @@
 package se.inte.group5;
 
 public class Creature {
+    private int currentLife, maxSpeed;
 
-    //public Creature()
+    public Creature(int currentLife, int maxSpeed) {
+     this.currentLife = currentLife;
+     this.maxSpeed = maxSpeed;
+    }
+
+    public int getCurrentLife(){
+        return currentLife;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
 }
