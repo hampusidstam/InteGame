126aa147d0a39dad674edb3f793b0200dbe09805
loha4254 <lovisa.hanserkers@gmail.com>
2018-10-17 09:31:19 +0200

    Created test cases for Position class getX and getY methods and created those methods in Position class.

--- src/test/java/se/inte/group5/PositionTest.java
+++ src/test/java/se/inte/group5/PositionTest.java
@@ -1,16 +1,25 @@
 package se.inte.group5;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class PositionTest {
+    Position pos1;
 
-
-
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    @Before
+    public void createPositions(){
+        pos1 = new Position(5, 13);
     }
 
+    @Test
+    public void getX_xIs5_true(){
+        assertEquals(5, pos1.getX());
+    }
+
+    @Test
+    public void getY_yIs13_true(){
+        assertEquals(13, pos1.getY());
+    }
 }
