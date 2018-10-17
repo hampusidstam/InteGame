2909eba7109ff43c64861ed902c8c1cd175a962d
henwal <walker.henriette@gmail.com>
2018-10-17 12:47:59 +0200

    Added test class for Plant

---
+++ src/test/java/se/inte/group5/PlantTest.java
@@ -0,0 +1,35 @@
+package se.inte.group5;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class PlantTest {
+
+    private Plant p;
+
+    @Before
+    public void createPlant() {
+        p = new Plant(10, Color.RED);
+    }
+
+    @Test
+    public void getHealthPoints_hpIs10_true() {
+        assertEquals(10, p.getHealthPoints());
+    }
+
+    @Test
+    public void getHealthPoints_hpIs10_false() {
+        assertNotEquals(13, p.getHealthPoints());
+    }
+
+   /* @Test
+    public void getColor_colorIsRed_true() {
+        assertEquals("RED", p.getColor().toString());
+    } */
+
+}
