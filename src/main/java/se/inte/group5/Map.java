2dbabb4850e4ed3375ad49549743496e10afea3d
loha4254 <lovisa.hanserkers@gmail.com>
2018-10-17 12:50:34 +0200

    Added tests to test creation of map array and implemented the creation and get-method for the Map class.

--- src/main/java/se/inte/group5/Map.java
+++ src/main/java/se/inte/group5/Map.java
@@ -4,10 +4,13 @@ public class Map {
 
     private int width;
     private int height;
+    private GameObject[][] map;
 
     Map(int width, int height) {
         this.width = width;
         this.height = height;
+
+        createMap();
     }
 
     public int getWidth() {
@@ -16,5 +19,13 @@ public class Map {
 
     public int getHeight() {
         return this.height;
+    }
+
+    private void createMap(){
+        map = new GameObject[height][width];
+    }
+
+    public GameObject[][] getMap(){
+        return map;
     }
 }
--- src/test/java/se/inte/group5/MapTest.java
+++ src/test/java/se/inte/group5/MapTest.java
@@ -15,7 +15,18 @@ public class MapTest {
     }
 
     @Test
-    public void shouldReturnHeightOfMap() {
+    public void getWidth_widthIs200_False() {
         assertEquals(100, map.getHeight());
     }
+
+    @Test
+    public void createMap_width200_True(){
+        assertEquals(200, map.getMap()[0].length);
+    }
+
+    @Test
+    public void createMap_height100_True(){
+        assertEquals(100, map.getMap().length);
+    }
+
 }
