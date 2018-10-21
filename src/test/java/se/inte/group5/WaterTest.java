ca01aace1eff995d2c72faa1d0063841f85e9e2a
henwal <walker.henriette@gmail.com>
2018-10-21 11:04:31 +0200

    WaterTest and Water class added.

---
+++ src/test/java/se/inte/group5/WaterTest.java
@@ -0,0 +1,28 @@
+package se.inte.group5;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterTest {
+
+    Water wt;
+
+    @Before
+    public void createWater() {
+        wt = new Water();
+    }
+
+    @Test
+    public void getSymbol_symbolIsW_true() {
+        assertEquals('~', wt.getSymbol());
+    }
+
+    @Test
+    public void getColor_colorIsBlue_true() {
+        assertEquals(Color.BLUE, wt.getColor());
+    }
+
+}
+
