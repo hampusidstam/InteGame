91b9e175c55d26f9584344e436139fd5fbe4025e
rubenwilhelmsen <rubenwilhelmsen@live.se>
2018-10-21 13:54:55 +0200

    Basic inventory class and tests.

--- src/main/java/se/inte/group5/Inventory.java
+++ src/main/java/se/inte/group5/Inventory.java
@@ -2,4 +2,14 @@ package se.inte.group5;
 
 public class Inventory {
 
+    private Item[] inventoryArray;
+
+    public Inventory(int slots) {
+        inventoryArray = new Item[slots];
+    }
+
+    public Item[] getInventoryArray() {
+        return inventoryArray;
+    }
+
 }
--- src/test/java/se/inte/group5/InventoryTest.java
+++ src/test/java/se/inte/group5/InventoryTest.java
@@ -1,4 +1,24 @@
 package se.inte.group5;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class InventoryTest {
+
+    Inventory inventory;
+
+    @Before
+    public void createInventory() {
+        inventory = new Inventory(5);
+    }
+
+    @Test
+    public void getInventoryArray_Empty5SlotInventory_true() {
+        Item[] expected = new Item[5];
+        Assert.assertArrayEquals(expected, inventory.getInventoryArray());
+    }
+
 }
